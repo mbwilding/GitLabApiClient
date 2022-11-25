1457e4596c677e8714e00223e4041d3958d1f05f
dudu <dudu@cnblogs.com>
2022-11-25 18:32:43 +0800

    Remove Codecov

--- test.ps1
+++ test.ps1
@@ -7,13 +7,6 @@ if ($dockerOSType -ieq 'linux') {
 
 $exitWithError = $LastExitCode -ne 0
 
-$openCoverFile = Get-ChildItem -Path "test/*/coverage.opencover.xml" -Recurse | Sort-Object LastWriteTime | Select-Object -last 1
-if (Test-Path "$openCoverFile") {
-  Write-Host "Uploading coverage file"
-  & codecov -f "$openCoverFile" -t bda1c835-c4a2-4a1a-8d38-999b9a8ea80b
-  $exitWithError = $exitWithError -or $LastExitCode -ne 0
-}
-
 if ($exitWithError) {
   exit 1
 }
